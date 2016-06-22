2da031f5539c3482ba5e496040e01caf4b15ebca
uramonk <kazura1985@gmail.com>
2016-06-22 14:59:21 +0900

    Add API status code and handle network error

--- app/src/main/java/com/uramonk/androidtemplateapp/error/APIError.java
+++ app/src/main/java/com/uramonk/androidtemplateapp/error/APIError.java
@@ -6,9 +6,50 @@ package com.uramonk.androidtemplateapp.error;
 public class APIError {
     public final int cod;
     public final String message;
+    public final APIStatus apiStatus;
 
     public APIError(int cod, String message) {
         this.cod = cod;
         this.message = message;
+        this.apiStatus = convertAPIStatus(cod);
+    }
+
+    private APIStatus convertAPIStatus(int cod) {
+        if (cod == APIStatus.BAD_RESPONSE.getValue()) {
+            return APIStatus.BAD_RESPONSE;
+        } else if (cod == APIStatus.NETWORK_ERROR.getValue()) {
+            return APIStatus.NETWORK_ERROR;
+        } else if (cod == APIStatus.BAD_REQUEST.getValue()) {
+            return APIStatus.BAD_REQUEST;
+        } else if (cod == APIStatus.UNAUTHORIZED.getValue()) {
+            return APIStatus.UNAUTHORIZED;
+        } else if (cod == APIStatus.PAYMENT_REQUIRED.getValue()) {
+            return APIStatus.PAYMENT_REQUIRED;
+        } else if (cod == APIStatus.FORBIDDEN.getValue()) {
+            return APIStatus.FORBIDDEN;
+        } else if (cod == APIStatus.NOT_FOUND.getValue()) {
+            return APIStatus.NOT_FOUND;
+        } else if (cod == APIStatus.METHOD_NOT_ALLOWED.getValue()) {
+            return APIStatus.METHOD_NOT_ALLOWED;
+        } else if (cod == APIStatus.NOT_ACCEPTABLE.getValue()) {
+            return APIStatus.NOT_ACCEPTABLE;
+        } else if (cod == APIStatus.PROXY_AUTHENTICATION_REQUIRED.getValue()) {
+            return APIStatus.PROXY_AUTHENTICATION_REQUIRED;
+        } else if (cod == APIStatus.REQUEST_TIMEOUT.getValue()) {
+            return APIStatus.REQUEST_TIMEOUT;
+        } else if (cod == APIStatus.CONFLICT.getValue()) {
+            return APIStatus.CONFLICT;
+        } else if (cod == APIStatus.GONE.getValue()) {
+            return APIStatus.GONE;
+        } else if (cod == APIStatus.INTERNAL_SERVER_ERROR.getValue()) {
+            return APIStatus.INTERNAL_SERVER_ERROR;
+        } else if (cod == APIStatus.NOT_IMPLEMENTED.getValue()) {
+            return APIStatus.NOT_IMPLEMENTED;
+        } else if (cod == APIStatus.BAD_GATEWAY.getValue()) {
+            return APIStatus.BAD_GATEWAY;
+        } else if (cod == APIStatus.SERVICE_UNAVAIABLE.getValue()) {
+            return APIStatus.SERVICE_UNAVAIABLE;
+        }
+        return APIStatus.UNKNOWN;
     }
 }
--- app/src/main/java/com/uramonk/androidtemplateapp/error/CommonErrorHandler.java
+++ app/src/main/java/com/uramonk/androidtemplateapp/error/CommonErrorHandler.java
@@ -17,29 +17,42 @@ import timber.log.Timber;
  */
 public class CommonErrorHandler {
     public static void handleError(Context context, Throwable throwable, Retrofit retrofit) {
+        // 400 ~ 500
         if (throwable instanceof HttpException) {
             handleHttpException(context, (HttpException) throwable, retrofit);
         }
+        // NETWORK_ERROR
+        // https://github.com/square/retrofit/issues/1260#issuecomment-154878865
+        else if(throwable instanceof IOException){
+            handleIOException(context, (IOException) throwable);
+        }
     }
 
-    private static void handleHttpException(Context context, HttpException httpException, Retrofit retrofit) {
+    private static void handleHttpException(Context context, HttpException exception, Retrofit retrofit) {
         Converter<ResponseBody, APIError> errorConverter = retrofit.responseBodyConverter(APIError.class, new Annotation[0]);
         APIError apiError;
         try {
-            apiError = errorConverter.convert(httpException.response().errorBody());
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            builder.setTitle("Error: " + apiError.cod);
-            builder.setMessage(apiError.message);
-            builder.setPositiveButton("OK",
-                    (dialog, which) -> {
-                    })
-                    .create()
-                    .show();
+            apiError = errorConverter.convert(exception.response().errorBody());
         } catch (IOException e) {
-            Timber.e(e, "Cannot convert API Error.");
+            apiError = new APIError(APIStatus.BAD_RESPONSE.getValue(), e.getMessage());
         }
 
+        show(context, apiError);
+    }
 
+    private static void handleIOException(Context context, IOException exception) {
+        APIError apiError = new APIError(APIStatus.NETWORK_ERROR.getValue(), exception.getMessage());
+        show(context, apiError);
+    }
+
+    private static void show(Context context, APIError apiError) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Error: " + apiError.apiStatus);
+        builder.setMessage(apiError.message);
+        builder.setPositiveButton("OK",
+                (dialog, which) -> {
+                })
+                .create()
+                .show();
     }
 }
