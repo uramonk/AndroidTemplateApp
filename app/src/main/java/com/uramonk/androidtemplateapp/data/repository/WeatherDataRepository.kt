79e20a14e32a6c4b0c8ad7a79bdc731b78fc0a81
Kazunori Urano <kazura1985@gmail.com>
2017-03-06 23:42:19 +0900

    Retry access when api status code is unauthorized

--- app/src/main/java/com/uramonk/androidtemplateapp/data/repository/WeatherDataRepository.kt
+++ app/src/main/java/com/uramonk/androidtemplateapp/data/repository/WeatherDataRepository.kt
@@ -3,10 +3,14 @@ package com.uramonk.androidtemplateapp.data.repository
 import com.uramonk.androidtemplateapp.Constants
 import com.uramonk.androidtemplateapp.data.api.WeatherApi
 import com.uramonk.androidtemplateapp.data.entity.mapper.WeatherListEntityDataMapper
+import com.uramonk.androidtemplateapp.data.error.ApiStatus
 import com.uramonk.androidtemplateapp.domain.model.WeatherList
 import com.uramonk.androidtemplateapp.domain.repository.WeatherRepository
 
 import io.reactivex.Observable
+import io.reactivex.functions.Function
+import retrofit2.HttpException
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by uramonk on 2016/06/29.
@@ -17,6 +21,25 @@ class WeatherDataRepository(private val weatherApi: WeatherApi,
     override fun getWeatherList(): Observable<WeatherList> {
         return weatherApi.getWeather("TOKYO", Constants.OPEN_WEATHER_MAP_API_KEY).map {
             weatherListEntityDataMapper.transform(it)
+        }.retryWhen(retryWhenUnAuthorized())
+    }
+
+    private fun retryWhenUnAuthorized(): Function<Observable<out Throwable>, Observable<*>> {
+        return Function { observable ->
+            observable.take(2).flatMap<HttpException> { it ->
+                if (it is HttpException) {
+                    val httpException: HttpException = it
+                    Observable.just(httpException)
+                } else {
+                    Observable.error { it }
+                }
+            }.flatMap<Any> { it ->
+                if (it.code() == ApiStatus.UNAUTHORIZED.value) {
+                    Observable.timer(3000, TimeUnit.MILLISECONDS);
+                } else {
+                    Observable.error { it }
+                }
+            }
         }
     }
 }
