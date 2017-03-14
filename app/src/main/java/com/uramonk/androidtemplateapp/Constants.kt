1348918028f63550fc1dfa7ab7c7274ae78cb505
Kazunori Urano <kazura1985@gmail.com>
2017-03-15 01:43:27 +0900

    Add Weather Map API key to local.properties

--- app/src/main/java/com/uramonk/androidtemplateapp/Constants.kt
+++ app/src/main/java/com/uramonk/androidtemplateapp/Constants.kt
@@ -8,5 +8,9 @@ object Constants {
      * OpenWeatherMap
      */
     val OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/"
-    val OPEN_WEATHER_MAP_API_KEY = "Your OpenWeatherMap API Key"
+
+    /**
+     * Must define `weatherMapApiKey` in local.properties
+     */
+    val OPEN_WEATHER_MAP_API_KEY = BuildConfig.WEATHER_MAP_API_KEY
 }
